41afcecda69f38253e795405ad0db71a5c5004b0
VeryLoveLoli <chuancan@icloud.com>
2021-11-27 13:27:29 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "LayerView",
     platforms: [
-        .macCatalyst(.v13), .iOS(.v12)
+        .macCatalyst(.v13),
+        .iOS(.v12),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
